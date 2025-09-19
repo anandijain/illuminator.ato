81f955bf7f1a4138996f3ba87ea465446d653fc2
Anand Jain <anandj@uchicago.edu>
2025-09-19 16:23:29 -0700

    all together now

--- mc_dc2_test/lcd.py
+++ mc_dc2_test/lcd.py
@@ -1,12 +1,55 @@
 from machine import Pin, SPI, PWM
 import time
 import st7789py as st7789
-import vga1_8x8 as font
+# import vga1_8x8 as font
+# import vga1_16x16 as font
+import vga2_16x32 as font
+import math
+
+OPTO_PIN = Pin(16, Pin.OUT)
+FET_PIN  = Pin(17, Pin.OUT)
+LED      = Pin("LED", Pin.OUT)
+
+def on_off(p, delay=0.1):
+    p.value(1)
+    time.sleep(delay)
+    p.value(0)
+    time.sleep(delay)
+
+def snap():
+    # Example: gate power on, pulse opto, gate off
+    FET_PIN.value(1)
+    on_off(OPTO_PIN, delay=0.3)
+    FET_PIN.value(0)
+    LED.toggle()  # visual heartbeat
+
+def timelapse(interval, duration):
+    """
+    interval: seconds between snaps (float ok)
+    duration: total run time in HOURS, or the string "forever"
+    """
+    if duration == "forever":
+        next_t = time.ticks_ms()
+        while True:
+            snap()
+            next_t = time.ticks_add(next_t, int(interval * 1000))
+            time.sleep_ms(max(0, time.ticks_diff(next_t, time.ticks_ms())))
+        # never returns
+
+    dur_sec = duration * 3600  # duration is in HOURS per docstring
+    num_snaps = math.ceil(dur_sec / interval)
+
+    next_t = time.ticks_ms()
+    for i in range(num_snaps):
+        snap()
+        if i == num_snaps - 1:
+            break  # don't sleep after last snap
+        next_t = time.ticks_add(next_t, int(interval * 1000))
+        time.sleep_ms(max(0, time.ticks_diff(next_t, time.ticks_ms())))
 
 params = {"interval [s]": 30, "duration [hr]": 36}
 param_keys = list(params)
 N_PARAMS = len(params)
-TEXT_H = TEXT_W = 8
 
 spi = SPI(1, baudrate=40_000_000, polarity=0, phase=0,
           sck=Pin(10), mosi=Pin(11))
@@ -27,11 +70,12 @@ cursor_idx = 0
 def draw():
     for i, key in enumerate(param_keys):
         prefix = "> " if i == cursor_idx else "  "
-        tft.text(font, "{}{}: {}".format(prefix, key, params[key]), 0, i*TEXT_H)
+        tft.text(font, "{}{}: {}".format(prefix, key, params[key]), 0, i*font.HEIGHT)
 draw()
 
 change_delta = 0   # -1, 0, +1 pending changes for current field
 next_pending = False
+start_pending = False
 
 def incr_cb(p):
     global change_delta
@@ -45,32 +89,46 @@ def next_cb(p):
     global next_pending
     next_pending = True
 
-decr = Pin(20, Pin.IN, Pin.PULL_UP)
-incr = Pin(19, Pin.IN, Pin.PULL_UP)
-next = Pin(18, Pin.IN, Pin.PULL_UP)
+def start_cb(p):
+    global start_pending
+    start_pending = True
+    # print("starting timelapse")
+
+
+decr = Pin(21, Pin.IN, Pin.PULL_UP)
+incr = Pin(20, Pin.IN, Pin.PULL_UP)
+next = Pin(19, Pin.IN, Pin.PULL_UP)
+start = Pin(18, Pin.IN, Pin.PULL_UP)
 
 decr.irq(trigger=Pin.IRQ_RISING, handler=decr_cb)
 incr.irq(trigger=Pin.IRQ_RISING, handler=incr_cb)
 next.irq(trigger=Pin.IRQ_RISING, handler=next_cb)
+start.irq(trigger=Pin.IRQ_RISING, handler=start_cb)
 
 last_apply = time.ticks_ms()
 APPLY_EVERY_MS = 30   # small debounce / coalesce window
 
 while True:
-    now = time.ticks_ms()
+    try: 
+        now = time.ticks_ms()
 
-    # handle "next" (change selected field)
-    if next_pending:
-        next_pending = False
-        cursor_idx = (cursor_idx + 1) % N_PARAMS
-        draw()
+        # handle "next" (change selected field)
+        if next_pending:
+            next_pending = False
+            cursor_idx = (cursor_idx + 1) % N_PARAMS
+            draw()
+        if start_pending: 
+            next_pending = False
+            tft.text(font, "starting timelapse...", 0, (N_PARAMS+1)*font.HEIGHT)
+            timelapse(params["interval [s]"], params["duration [hr]"])
+        # coalesce +/- presses and apply periodically (debounce-ish)
+        if change_delta != 0 and time.ticks_diff(now, last_apply) >= APPLY_EVERY_MS:
+            key = param_keys[cursor_idx]
+            params[key] += change_delta
+            change_delta = 0
+            last_apply = now
+            draw()
 
-    # coalesce +/- presses and apply periodically (debounce-ish)
-    if change_delta != 0 and time.ticks_diff(now, last_apply) >= APPLY_EVERY_MS:
-        key = param_keys[cursor_idx]
-        params[key] += change_delta
-        change_delta = 0
-        last_apply = now
-        draw()
-
-    time.sleep_ms(5)
+        time.sleep_ms(5)
+    except KeyboardInterrupt:
+        break
